1160218af219d3c14577a163a4b2c720ca40c9a1
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,36 @@ output "consumption_budget_resource_groups" {
   description = "All consumption_budget_resource_group resources"
   value       = azurerm_consumption_budget_resource_group.consumption_budget_resource_groups
 }
+output "consumption_budget_resource_groups_amount" {
+  description = "List of amount values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.amount]
+}
+output "consumption_budget_resource_groups_etag" {
+  description = "List of etag values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.etag]
+}
+output "consumption_budget_resource_groups_filter" {
+  description = "List of filter values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.filter]
+}
+output "consumption_budget_resource_groups_name" {
+  description = "List of name values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.name]
+}
+output "consumption_budget_resource_groups_notification" {
+  description = "List of notification values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.notification]
+}
+output "consumption_budget_resource_groups_resource_group_id" {
+  description = "List of resource_group_id values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.resource_group_id]
+}
+output "consumption_budget_resource_groups_time_grain" {
+  description = "List of time_grain values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.time_grain]
+}
+output "consumption_budget_resource_groups_time_period" {
+  description = "List of time_period values across all consumption_budget_resource_groups"
+  value       = [for k, v in azurerm_consumption_budget_resource_group.consumption_budget_resource_groups : v.time_period]
+}
 
